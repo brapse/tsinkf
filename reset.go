1ac6363b20aa25056ba36550437aaa3618313fb5
Sean Braithwaite <brapse@gmail.com>
2013-07-05 17:13:53 +0200

    Fix reset:
      + fix bug in which reset flags would be parsed
      + add state flag that allows any state to be set
      + add -all flag to operates on all jobs but requires
        a stdin confirmation

--- reset.go
+++ reset.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+func contains(needle string, heystack []string) int {
+	for i, possible := range heystack {
+		if needle == possible {
+			return i
+		}
+	}
+
+	return -1
+}
+
 var resetFn CmdFn = func(c *Cmd, args []string) int {
 	verbose := c.Flags.Bool("v", false, "verbose output")
-	state := c.Flags.String("s", STATELABELS[NEW], "state to set")
+	state := c.Flags.String("state", STATELABELS[NEW], "state to set")
+	all := c.Flags.Bool("all", false, "operate on all jobs")
+
+	c.Flags.Parse(args)
 
 	NEWSTATE, found := JOBSTATEIDS[*state]
 	if !found {
@@ -18,13 +35,32 @@ var resetFn CmdFn = func(c *Cmd, args []string) int {
 	jobIDs := c.Flags.Args()
 
 	jobList := NewJobList(store, journal)
-	resetable := JobSpecific(jobIDs)
+
+	if *all {
+
+		for _, job := range jobList {
+			fmt.Println(job.ToString())
+		}
+
+		fmt.Printf("Resetting all jobs to %s, type \"yes\" to confirm: ", STATELABELS[NEWSTATE])
+		var input string
+		_, err := fmt.Scanf("%s", &input)
+		if err != nil {
+			panic(err)
+		}
+
+		if input != "yes" {
+			fmt.Println("ABORT")
+			return 1
+		}
+	}
 
 	for _, job := range jobList {
-		if resetable(job) {
+		if *all || contains(job.id, jobIDs) > -1 {
 			job.SetState(NEWSTATE)
 		}
 	}
+
 	return 0
 }
 
